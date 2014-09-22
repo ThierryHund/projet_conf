a9d5fba5727b6d08c149e5100ff19a8b6d4dd1c0
Thebaud <thebaud.clement@gmail.com>
2014-09-22 16:51:56 +0200

    Modif pour connexion

--- projet_conf/server/controler.php
+++ projet_conf/server/controler.php
@@ -3,4 +3,11 @@ require_once "connection.class.php";
 
 $conn = Connection::get ();
 
+if(isset($_POST['connexion']))
+{
+	if(isset($_POST['login']) && isset($_POST['password'])){
+		
+	}
+}
+
 print "kklklkl";
