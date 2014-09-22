0ff49e5e69142a467188dd38afc7d2c6c179a51a
thebaud2u <thebaud.clement@gmail.com>
2014-09-22 19:43:34 +0200

    modif controler etc...

--- projet_conf/server/controler.php
+++ projet_conf/server/controler.php
@@ -1,5 +1,5 @@
 <?php
-require_once "connection.class.php";
+require_once "modele\dao\connection.class.php";
 
 $conn = Connection::get ();
 
@@ -8,18 +8,22 @@ if(isset($_POST['connexion']))
 	if(isset($_POST['login']) && isset($_POST['password'])){
 		$login=$_POST['login'];
 		$password=$_POST['password'];
+            $hash = crypt($password);
+
+            echo 'SELECT *
+            FROM admin
+            WHERE admin.identifiant_admin LIKE "'.$login.'"
+            AND admin.mdp_admin LIKE "'.$hash.'"';
 
 		$verif_login = $conn->query('SELECT *
             FROM admin
             WHERE admin.identifiant_admin LIKE "'.$login.'"
-            AND admin.mdp_admin LIKE "'.$password);
+            AND admin.mdp_admin LIKE "'.$hash.'"');
 
             $liste = $verif_login->fetchAll();
             if (count($liste) == 0) { 
-            	echo 'Erreur dans l\'identifiant ou dans le mot de passe.';
+            	echo 'ta race';
             }
-            else header('location:accueil.html');
+            else echo 'couille';
 	}
 }
-
-print "kklklkl";
