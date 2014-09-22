dbb7bddcb9d674d025f51a1ed6be6e076a9a3672
Thebaud <thebaud.clement@gmail.com>
2014-09-22 17:03:01 +0200

    Re modif pour connexion et ajout d'accueil administration

--- projet_conf/server/controler.php
+++ projet_conf/server/controler.php
@@ -6,7 +6,19 @@ $conn = Connection::get ();
 if(isset($_POST['connexion']))
 {
 	if(isset($_POST['login']) && isset($_POST['password'])){
-		
+		$login=$_POST['login'];
+		$password=$_POST['password'];
+
+		$verif_login = $conn->query('SELECT *
+            FROM admin
+            WHERE admin.identifiant_admin LIKE "'.$login.'"
+            AND admin.mdp_admin LIKE "'.$password);
+
+            $liste = $verif_login->fetchAll();
+            if (count($liste) == 0) { 
+            	echo 'Erreur dans l\'identifiant ou dans le mot de passe.';
+            }
+            else header('location:accueil.html');
 	}
 }
 
