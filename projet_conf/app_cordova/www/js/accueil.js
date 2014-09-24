fc09f8a9618e36f10dda9a444622de2ca075600f
HundThierry <tyery1@gmail.com>
2014-09-24 21:02:58 +0200

    Modif accueil.js et ajout de jquerymobile (appli non fonctionnelle sur mobile)

--- projet_conf/app_cordova/www/js/accueil.js
+++ projet_conf/app_cordova/www/js/accueil.js
@@ -1,3 +1,5 @@
+$(document).on('pagecreate',function(){
+
 $.get( "http://localhost/webprojet/projet_conf/server/controler.php",'accueil',function( data ) {
 
 //ajout titre evenement
@@ -5,5 +7,5 @@ $.get( "http://localhost/webprojet/projet_conf/server/controler.php",'accueil',f
   //ajout description evt
   $( 'body .container .row:nth-child(2) .col-lg-12' ).html( data['desc_evnt'] );
   //ajout logo
-  $( 'body .container .row:nth-child(1) .col-sm-6:nth-child(1)' ).html('<img src="'+data['logo']+'">' )
-},"json");
+  $( 'body .container .row:nth-child(1) .col-sm-6:nth-child(1)' ).html('<img class="img-responsive" src="'+data['logo']+'">' )
+},"json");});
