baa3b537eb3ab5877381bcb72b327b2c8e299b0b
thebaud2u <thebaud.clement@gmail.com>
2014-09-24 12:44:14 +0200

    modif controler.

--- projet_conf/server/controler.php
+++ projet_conf/server/controler.php
@@ -10,23 +10,17 @@ if(isset($_POST['connexion']))
 	if(isset($_POST['login']) && isset($_POST['password'])){
 		$login=$_POST['login'];
 		$password=$_POST['password'];
-            $hash = crypt($password);
-
-            echo 'SELECT *
-            FROM admin
-            WHERE admin.identifiant_admin LIKE "'.$login.'"
-            AND admin.mdp_admin LIKE "'.$hash.'"';
 
 		$verif_login = $conn->query('SELECT *
             FROM admin
             WHERE admin.identifiant_admin LIKE "'.$login.'"
-            AND admin.mdp_admin LIKE "'.$hash.'"');
+            AND admin.mdp_admin LIKE "'.$password.'"');
 
             $liste = $verif_login->fetchAll();
             if (count($liste) == 0) { 
-            	echo 'ta race';
+            	header('location:..\app_cordova\www\connexion.html')
             }
-            else echo 'couille';
+            else //lancement variable session
 	}
 }
 
