fb98c58b28b3fbefe7897d47133b968410b25ea2
MaximeBoubel <boubel.maxime@gmail.com>
2014-09-25 11:27:16 +0200

    modif accueil et index pour planning

--- projet_conf/app_cordova/www/js/accueil.js
+++ projet_conf/app_cordova/www/js/accueil.js
@@ -8,3 +8,19 @@ $.get( "http://127.0.0.1/projet_conf/projet_conf/server/controler.php",'accueil'
   //ajout logo
   $( 'body .container .row:nth-child(1) .col-sm-6:nth-child(1)' ).html('<img class="img-responsive" src="'+data['logo']+'">' )
 },"json");});
+
+$(document).on('ready',function(){
+
+$.get( "http://127.0.0.1/projet_conf/projet_conf/server/controler.php",'description_presentation',function( data ) {
+
+/*	//ajout date jour 1 presentation
+  $( 'body .container .row:nth-child(4) .col-lg-12 panel:nth-child(1) .panel panel-default:nth-child(1) .panel-heading .panel-title .a #date_jour1' ).html( data['date_presentation'] );
+  */
+  //ajout date jour 1 presentation
+  $( 'body .container .row:nth-child(4) span.date_jour1' ).html( data['date_presentation'] );
+	//ajout titre presentation
+  $( 'body .container #collapseOne .panel-body .titre_pres' ).html( data['titre_presentation'] );
+  //ajout description presentation
+  $( 'body .container #collapseOne .panel-body .description' ).html( data['description'] )
+  
+},"json");});
