d21abf039a07fbf56c98c26ef3747bcf00ade24d
michita <giannellimic@msn.com>
2014-09-25 11:02:22 +0200

    modify page contact

---
+++ projet_conf/app_cordova/www/js/success-msg.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+	$( "form" ).bind("submit",function() {
+
+	  	$( "#success" ).css(
+			{
+
+			    opacity: 1
+			},
+			{ 
+
+			    display: "block"
+			});
+
+	});
+});
+
