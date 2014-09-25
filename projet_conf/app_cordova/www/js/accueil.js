9f893dcb1e6457374e6caf1f7f724bb896291739
HundThierry <tyery1@gmail.com>
2014-09-25 09:55:38 +0200

    ajout classes bootstrap pour planning accueil

--- projet_conf/app_cordova/www/js/accueil.js
+++ projet_conf/app_cordova/www/js/accueil.js
@@ -1,11 +1,11 @@
 $(document).on('ready',function(){
-
-$.get( "http://localhost/webprojet/projet_conf/server/controler.php",'accueil',function( data ) {
-
-//ajout titre evenement
-  $( '.navbar-brand' ).html( data['titre_evnt'] );
-  //ajout description evt
-  $( 'body .container .row:nth-child(2) .col-lg-12' ).html( data['desc_evnt'] );
-  //ajout logo
-  $( 'body .container .row:nth-child(1) .col-sm-6:nth-child(1)' ).html('<img class="img-responsive" src="'+data['logo']+'">' )
-},"json");});
+	$.get( "http://localhost/webprojet/projet_conf/server/controler.php",'accueil',function( data ) {
+		//ajout titre evenement
+		$( '.navbar-brand' ).html( data['titre_evnt'] );
+		//ajout description evt
+		$( 'body .container .row:nth-child(2) .col-lg-12' ).html( data['desc_evnt'] );
+		//ajout logo
+		$( 'body .container .row:nth-child(1) .col-sm-6:nth-child(1)' ).html('<img class="img-responsive" src="'+data['logo']+'">' )
+		},"json");
+	
+});
