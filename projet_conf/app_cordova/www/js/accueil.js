27183bcf575adb71c7b9a1dab974855eb6a5355e
MaximeBoubel <boubel.maxime@gmail.com>
2014-09-25 11:39:13 +0200

    la même

--- projet_conf/app_cordova/www/js/accueil.js
+++ projet_conf/app_cordova/www/js/accueil.js
@@ -13,9 +13,7 @@ $(document).on('ready',function(){
 
 $.get( "http://127.0.0.1/projet_conf/projet_conf/server/controler.php",'description_presentation',function( data ) {
 
-/*	//ajout date jour 1 presentation
-  $( 'body .container .row:nth-child(4) .col-lg-12 panel:nth-child(1) .panel panel-default:nth-child(1) .panel-heading .panel-title .a #date_jour1' ).html( data['date_presentation'] );
-  */
+
   //ajout date jour 1 presentation
   $( 'body .container .row:nth-child(4) span.date_jour1' ).html( data['date_presentation'] );
 	//ajout titre presentation
