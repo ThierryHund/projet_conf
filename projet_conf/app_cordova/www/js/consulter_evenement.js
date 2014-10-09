883f15221119df9c0c21923ae8eff3de5594dec9
michita <giannellimic@msn.com>
2014-10-09 14:08:30 +0200

    ajout boutons

--- projet_conf/app_cordova/www/js/consulter_evenement.js
+++ projet_conf/app_cordova/www/js/consulter_evenement.js
@@ -1,8 +1,8 @@
 $.get( "http://localhost/webprojet/projet_conf/projet_conf/server/controler_admin.php",'consulter_evenement',function( data ) {
   $.each(data, function() {
 	    $('.info').append(
-	        '<tr><td>'
-	        // + this.id+ '</td><td>'
+	        '<tr><td class="hidden">'
+	        + this.id+ '</td><td>'
 	        + this.titre +'</td><td>'
 	        + this.heure_deb+ '</td><td>'
 	        + this.heure_fin +'</td><td>'
@@ -13,7 +13,7 @@ $.get( "http://localhost/webprojet/projet_conf/projet_conf/server/controler_admi
 	        // + '<button type="button" class="btn btn-default btn-xs"> <span class="glyphicon glyphicon-pencil" </span> </button>'
 	        // +'<button type="button" class="btn btn-default btn-xs"> <span class="glyphicon glyphicon-remove" </span> </button>'
  
-	        +'<div class="btn-group"><button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown"> Action <span class="caret" ></span></button> <ul class="dropdown-menu" role="menu"> <li><a href="ajout_presentation.html">Ajouter présentation</a></li> <li><a href="#">Editer</a></li> <li class="divider"></li> <li><a href="#">Supprimer</a></li> </ul></div>'
+	        +'<div class="btn-group"><button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown"> Action <span class="caret" ></span></button> <ul class="dropdown-menu" role="menu"> <li><a href="ajout_presentation.html">Consulter / Editer </a></li> <li class="divider"></li> <li><a href="#" id="suppr">Supprimer</a></li> </ul></div>'
 	        +'</td></tr>'
 
 
--- projet_conf/app_cordova/www/js/consulter_presentation.js
+++ projet_conf/app_cordova/www/js/consulter_presentation.js
@@ -1,13 +1,20 @@
 $.get( "http://localhost/webprojet/projet_conf/projet_conf/server/controler_admin.php",'consulter_presentation',function( data ) {
 	$.each(data, function() {
-	    $('.info').append(
-	        '<tr><td>'
+	        '<tr><td class="hidden">'
 	        + this.id+ '</td><td>'
 	        + this.titre +'</td><td>'
-	        + this.description+ '</td><td>'
-	        + this.nom_orateur +'</td><td>'
-	        + this.prenom_orateur + '</td><td>'
-	        + this.nom_entreprise +'</td></tr>'
+	        + this.heure_deb+ '</td><td>'
+	        + this.heure_fin +'</td><td>'
+	        + this.date_deb + '</td><td>'
+	        + this.date_fin+ '</td><td>'
+	        + this.adresse +'</td><td>'
+	        + this.description +'</td><td>'
+	        // + '<button type="button" class="btn btn-default btn-xs"> <span class="glyphicon glyphicon-pencil" </span> </button>'
+	        // +'<button type="button" class="btn btn-default btn-xs"> <span class="glyphicon glyphicon-remove" </span> </button>'
+ 
+	        +'<div class="btn-group"><button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown"> Action <span class="caret" ></span></button> <ul class="dropdown-menu" role="menu"> <li><a href="ajout_presentation.html">Consulter / Editer </a></li> <li class="divider"></li> <li><a href="#" id="suppr">Supprimer</a></li> </ul></div>'
+	        +'</td></tr>'
+
 	    );
 	});
 	
