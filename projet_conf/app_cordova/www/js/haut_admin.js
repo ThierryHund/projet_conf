3fde2a7524ff1150296356fd469e0ded01e5db9d
michita <giannellimic@msn.com>
2014-10-11 16:41:54 +0200

    Ajout header admin.'s + design responsive

---
+++ projet_conf/app_cordova/www/js/haut_admin.js
@@ -0,0 +1,39 @@
+document.write(
+"	<div class='navbar navbar-default' role='navigation'>" +
+"		<div class='container'>" +
+"			<div class='navbar-header'>" +
+"				<button type='button' class='navbar-toggle' data-toggle='collapse' data-target='.navbar-collapse'>" +
+"					<span class='icon-bar'></span>" +
+"					<span class='icon-bar'></span>" +
+"					<span class='icon-bar'></span>" +				
+"				</button>" +
+"				<a class='navbar-brand' href='http://localhost/webprojet/projet_conf/projet_conf/app_cordova/www/index.html'></a>" +
+"			</div>" +
+"			<div class='collapse navbar-collapse'>" +
+"				<ul class='nav navbar-nav'>" +
+"					<li class='active'><a href='accueil.html'>Accueil</a></li>" +
+"					    <li class='dropdown'>" +
+"             				<a href='#' class='dropdown-toggle' data-toggle='dropdown'>Ajouter<span class='caret'></span></a>" +
+"	              				<ul class='dropdown-menu' role='menu'> " +
+"	                				<li><a href='ajout_evenement.html'>Ajouter un événement</a></li>" +
+"	                				<li class='divider'></li> " +
+"	                				<li><a href='ajout_presentation.html'>Ajouter une présentation</a></li>" +
+"	              				</ul>" +
+"           			</li>" +
+"			            <li class='dropdown'>" +
+"			              <a href='#' class='dropdown-toggle' data-toggle='dropdown'>Consulter<span class='caret'></span></a>" +
+"			              <ul class='dropdown-menu' role='menu'>" +
+"			                <li><a href='consulter_evenement.html'>Consulter un événement</a></li>" +
+"			                <li class='divider'></li>" +
+"			                <li><a href='consulter_presentation.html'>Consulter une présentation</a></li>" +
+"			              </ul>" +
+"			            </li>"+
+"				</ul>" +
+"				 <ul class='nav navbar-nav navbar-right'>" +
+"            			<li><a href='index.html'>Déconnexion</a></li>" +
+"         		 </ul>" +
+"			</div>" +
+"			<!--/.nav-collapse -->" +
+"		</div>" +
+"	</div>" 
+);
