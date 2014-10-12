2c937826e3f5846e99ab83b9e7e6de49c3f7497b
MaximeBoubel <boubel.maxime@gmail.com>
2014-10-12 09:56:20 +0200

    desc pres style test

--- projet_conf/app_cordova/www/js/description_presentation.js
+++ projet_conf/app_cordova/www/js/description_presentation.js
@@ -10,26 +10,20 @@ $.get( "http://localhost/webprojet/projet_conf/projet_conf/server/controler.php"
 		$( '.titre' ).html( data[0]['titre_presentation'] );
 	
 		//ajout date
-		$( '.date' ).append( '<div>'+"Le "+data[0]['date_presentation']+'</div>' ); 
+		$( '.date' ).append( '<div>'+"Le "+data[0]['date_presentation']+'</div><div>'+"De "+data[0]['heure_debut_presentation']+" à "+data[0]['heure_fin_presentation']+'</div>' ); 
 		
 		//ajout heure debut presentation
 		$( '.horaires' ).append( '<div>'+"De "+data[0]['heure_debut_presentation']+" à "+data[0]['heure_fin_presentation']+'</div>' );
 		
 		//ajout type de presentation
-		$( '.orateur' ).prepend( '<div>'+data[0]['type_presentation']+" présenté(e) par "+'</div><br/>' );
+		$( '.orateur' ).prepend( '<h4>'+data[0]['type_presentation']+" présenté(e) par "+'</h4><br/>' );
 		
 		$.each(data[0]['auteurs'], function(i, val){
-			$('.orateur' ).append( '<div class="text-center">'
+			$('.orateur' ).append( '<span class="">'
 									+val['prenom']+" "+val['nom']+
-									'</div>'+" "+'<a href='+val['url_entp']+'" target="_blank"><img src="'+val['logo_entp']+" class='img-thumbnail'></a>");
+									'</span>'+" "+'<a href="'+val['url_entp']+'" target="_blank"><img src="'+val['logo_entp']+'" class="img-responsive logo-entp"></a>');
 				 
 		});
-		
-		//ajout du logo
-		$.each(data[0]['auteurs'], function(i, val){
-			$( '.logo-entp' ).append('<a href="'+val['url_entp']+'" target="_blank"><img src="'+val['logo_entp']+'" class="img-responsive logo-entp"></a>')
-		});
-		
 		
 		//ajout description de la présentation
 		$( '.desc' ).html( data[0]['description'] )
