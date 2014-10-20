5857d47b11fd34f01945135815358094f6da6e3a
michita <giannellimic@msn.com>
2014-10-20 23:17:57 +0200

    Ajout bouton modifier un event + formulaire

--- projet_conf/app_cordova/www/js/modifier_evenement.js
+++ projet_conf/app_cordova/www/js/modifier_evenement.js
@@ -28,4 +28,34 @@
 		});
 
 	},"json");
+
+//envoi du formulaire
+$('form').submit(function(e){
+	e.preventDefault();
+	var formData= new FormData(this);
+	formData.append('modifier_evenement','modifier');
+	//console.log(formData);
+	$.ajax({
+		type: 'POST',
+		data: formData,
+		url: 'http://localhost/webprojet/projet_conf/projet_conf/server/controler_admin.php',
+		processData: false,
+		contentType: false,
+		success: function(){
+			
+			//alert('Your comment was successfully added');
+			document.location.href="http://localhost/webprojet/projet_conf/projet_conf/app_cordova/www/modifier_evenement.html?id="+id_evt;
+			
+			
+		},
+		error: function(){
+			console.log();
+			alert('error');
+		}
+	});
+	
+});
+
+
 ;})
+
